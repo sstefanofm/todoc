3206efc77779d6771314b65cfe52c8f7f0ecf533
stf <sstefanofm@gmail.com>
2024-06-16 20:56:02 -0300

    draw window

---
+++ todoc.c
@@ -0,0 +1,25 @@
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+int
+main(void)
+{
+  glfwInit();
+
+  GLFWwindow * window = glfwCreateWindow(700, 700, "Todo", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
