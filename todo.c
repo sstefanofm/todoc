9cdf189f31483afdb3c20a32999b8dde5c424d95
stf <sstefanofm@gmail.com>
2024-08-10 22:41:56 -0300

    start managing tabs

--- todo.c
+++ todo.c
@@ -8,6 +8,11 @@
 #define WIN_PADDING 20
 
 typedef enum {
+  DASHBOARD = 0,
+  NEW_TASK
+} GuiTab;
+
+typedef enum {
   ALL = 0,
   IN_PROGRESS,
   COMPLETED
@@ -26,7 +31,8 @@ typedef struct {
 } task;
 
 static uint16_t win_w = 640, win_h = 320;
-static Filter current_filter = 0;
+static Filter current_filter = ALL;
+static GuiTab current_tab = DASHBOARD;
 static LfFont title_font;
 static LfFont newtask_font;
 static LfFont filter_font;
@@ -60,7 +66,9 @@ render_header(void)
   lf_set_ptr_x_absolute(win_w - (WIN_PADDING * 2.0f) - btn_w);
 
   lf_push_font(&newtask_font);
-  lf_button_fixed("New task", btn_w, -1);
+  if (lf_button_fixed("New task", btn_w, -1) == LF_CLICKED) {
+    current_tab = NEW_TASK;
+  }
   lf_pop_font();
 
   lf_pop_style_props();
@@ -99,6 +107,139 @@ render_filters(void)
 
   lf_pop_font();
   lf_next_line();
+}
+
+static void
+render_tasks(void)
+{
+  LfUIElementProps div_props = lf_get_theme().div_props;
+  div_props.margin_left = 0.0f;
+  div_props.margin_top = 0.0f;
+  div_props.margin_right = 0.0f;
+  div_props.margin_bottom = 0.0f;
+  lf_push_style_props(div_props);
+
+  lf_div_begin(
+    ((vec2s) { lf_get_ptr_x(), lf_get_ptr_y() }),
+    ((vec2s) { win_w - (WIN_PADDING * 2.0f), win_h - lf_get_ptr_y() - WIN_PADDING }),
+    true
+  );
+
+  lf_pop_style_props();
+  lf_push_font(&task_font);
+
+  uint16_t num_rendered = 0;
+
+  /* draw task */
+  for (uint16_t i = 0; i < num_tasks; ++i, lf_next_line()) {
+    task * t = tasks[i];
+
+    if (current_filter == IN_PROGRESS && t->completed)
+      continue;
+    if (current_filter == COMPLETED && !t->completed)
+      continue;
+
+    ++num_rendered;
+
+    float ptr_y = lf_get_ptr_y() + 5.f;
+    const float inc_y = 16.5f;
+    const float priority_size = 11.f;
+    float margin_left = 15.f;
+
+    { /* draw priority badge */
+      LfColor priority_color;
+
+      switch (t->priority) {
+        case LOW:
+          priority_color = (LfColor) { 14, 168, 239, 255 };
+          break;
+        case MEDIUM:
+          priority_color = (LfColor) { 239, 200, 14, 255 };
+          break;
+        case HIGH:
+          priority_color = (LfColor) { 239, 14, 48, 255 };
+      }
+
+      lf_set_ptr_y_absolute(ptr_y += inc_y);
+      lf_rect(priority_size, priority_size, priority_color, 6.f);
+    }
+
+    { /* draw remove task trash can button */
+      LfUIElementProps trash_props = lf_get_theme().button_props;
+      trash_props.color = LF_NO_COLOR;
+      trash_props.border_width = 0.f;
+      trash_props.padding = 0.f;
+      trash_props.margin_top = 0.f;
+      trash_props.margin_left = 0.f;
+      trash_props.margin_right = 0.f;
+      trash_props.margin_bottom = 0.f;
+      lf_push_style_props(trash_props);
+
+      lf_set_ptr_y_absolute(ptr_y - 5.f);
+      lf_set_ptr_x(margin_left *= 1.2f);
+
+      if (lf_image_button(
+        ((LfTexture) { .id = trash_texture.id, .width = 20.f, .height = 20.f })
+      ) == LF_CLICKED) {
+        for (uint8_t task_index = i; task_index < num_tasks - 1; ++task_index)
+          *( tasks + i ) = *( tasks + i + 1 );
+        --num_tasks;
+      }
+
+      lf_pop_style_props();
+    }
+
+    { /* draw completed checkbox */
+      LfUIElementProps cb_props = lf_get_theme().checkbox_props;
+      cb_props.color = LF_NO_COLOR;
+      cb_props.border_width = .5f;
+      cb_props.border_color = (LfColor) { 66, 66, 66, 255 };
+      cb_props.corner_radius = 2.f;
+      lf_push_style_props(cb_props);
+
+      lf_set_ptr_x(margin_left *= 2.3f);
+      lf_set_ptr_y_absolute(ptr_y - inc_y);
+      lf_checkbox("", &tasks[i]->completed, LF_NO_COLOR, ((LfColor) { 65, 167, 204, 255 }));
+      lf_pop_style_props();
+
+      lf_next_line();
+    }
+
+    { /* draw description */
+      lf_set_ptr_y_absolute(ptr_y - inc_y);
+      lf_set_ptr_x(margin_left *= 2.f);
+      lf_text(tasks[i]->description);
+    }
+
+    { /* draw date */
+      LfUIElementProps date_props = lf_get_theme().text_props;
+      date_props.text_color = (LfColor) { 140, 140, 140, 255 };
+      lf_push_style_props(date_props);
+
+      lf_set_ptr_y_absolute(ptr_y);
+      lf_set_ptr_x(margin_left);
+      lf_text(tasks[i]->date);
+      lf_pop_style_props();
+    }
+
+    ptr_y += inc_y * 2;
+  }
+
+  if (!num_rendered) {
+    LfUIElementProps notask_props = lf_get_theme().text_props;
+    notask_props.text_color = (LfColor) { 177, 177, 177, 255 };
+    lf_push_style_props(notask_props);
+
+    lf_push_font(&filter_font);
+    lf_text("There is no tasks here... :(");
+    lf_pop_font(&filter_font);
+
+    lf_pop_style_props();
+  }
+
+  lf_pop_font();
+  lf_div_end();
+
 }
 
 int
@@ -150,134 +291,14 @@ main(void)
 
     render_header();
     render_filters();
-    {
-      LfUIElementProps div_props = lf_get_theme().div_props;
-      div_props.margin_left = 0.0f;
-      div_props.margin_top = 0.0f;
-      div_props.margin_right = 0.0f;
-      div_props.margin_bottom = 0.0f;
-      lf_push_style_props(div_props);
 
-      lf_div_begin(
-        ((vec2s) { lf_get_ptr_x(), lf_get_ptr_y() }),
-        ((vec2s) { win_w - (WIN_PADDING * 2.0f), win_h - lf_get_ptr_y() - WIN_PADDING }),
-        true
-      );
-
-      lf_pop_style_props();
-      lf_push_font(&task_font);
-
-      uint16_t num_rendered = 0;
-
-      /* draw task */
-      for (uint16_t i = 0; i < num_tasks; ++i, lf_next_line()) {
-        task * t = tasks[i];
-
-        if (current_filter == IN_PROGRESS && t->completed)
-          continue;
-        if (current_filter == COMPLETED && !t->completed)
-          continue;
-
-        ++num_rendered;
-
-        float ptr_y = lf_get_ptr_y() + 5.f;
-        const float inc_y = 16.5f;
-        const float priority_size = 11.f;
-        float margin_left = 15.f;
-
-        { /* draw priority badge */
-          LfColor priority_color;
-
-          switch (t->priority) {
-            case LOW:
-              priority_color = (LfColor) { 14, 168, 239, 255 };
-              break;
-            case MEDIUM:
-              priority_color = (LfColor) { 239, 200, 14, 255 };
-              break;
-            case HIGH:
-              priority_color = (LfColor) { 239, 14, 48, 255 };
-          }
-
-          lf_set_ptr_y_absolute(ptr_y += inc_y);
-          lf_rect(priority_size, priority_size, priority_color, 6.f);
-        }
-
-        { /* draw remove task trash can button */
-          LfUIElementProps trash_props = lf_get_theme().button_props;
-          trash_props.color = LF_NO_COLOR;
-          trash_props.border_width = 0.f;
-          trash_props.padding = 0.f;
-          trash_props.margin_top = 0.f;
-          trash_props.margin_left = 0.f;
-          trash_props.margin_right = 0.f;
-          trash_props.margin_bottom = 0.f;
-          lf_push_style_props(trash_props);
-
-          lf_set_ptr_y_absolute(ptr_y - 5.f);
-          lf_set_ptr_x(margin_left *= 1.2f);
-
-          if (lf_image_button(
-            ((LfTexture) { .id = trash_texture.id, .width = 20.f, .height = 20.f })
-          ) == LF_CLICKED) {
-            for (uint8_t task_index = i; task_index < num_tasks - 1; ++task_index)
-              *( tasks + i ) = *( tasks + i + 1 );
-            --num_tasks;
-          }
-
-          lf_pop_style_props();
-        }
-
-        { /* draw completed checkbox */
-          LfUIElementProps cb_props = lf_get_theme().checkbox_props;
-          cb_props.color = LF_NO_COLOR;
-          cb_props.border_width = .5f;
-          cb_props.border_color = (LfColor) { 66, 66, 66, 255 };
-          cb_props.corner_radius = 2.f;
-          lf_push_style_props(cb_props);
-
-          lf_set_ptr_x(margin_left *= 2.3f);
-          lf_set_ptr_y_absolute(ptr_y - inc_y);
-          lf_checkbox("", &tasks[i]->completed, LF_NO_COLOR, ((LfColor) { 65, 167, 204, 255 }));
-          lf_pop_style_props();
-
-          lf_next_line();
-        }
-
-        { /* draw description */
-          lf_set_ptr_y_absolute(ptr_y - inc_y);
-          lf_set_ptr_x(margin_left *= 2.f);
-          lf_text(tasks[i]->description);
-        }
-
-        { /* draw date */
-          LfUIElementProps date_props = lf_get_theme().text_props;
-          date_props.text_color = (LfColor) { 140, 140, 140, 255 };
-          lf_push_style_props(date_props);
-
-          lf_set_ptr_y_absolute(ptr_y);
-          lf_set_ptr_x(margin_left);
-          lf_text(tasks[i]->date);
-          lf_pop_style_props();
-        }
-
-        ptr_y += inc_y * 2;
-      }
-
-      if (!num_rendered) {
-        LfUIElementProps notask_props = lf_get_theme().text_props;
-        notask_props.text_color = (LfColor) { 177, 177, 177, 255 };
-        lf_push_style_props(notask_props);
-
-        lf_push_font(&filter_font);
-        lf_text("There is no tasks here... :(");
-        lf_pop_font(&filter_font);
-
-        lf_pop_style_props();
-      }
-
-      lf_pop_font();
-      lf_div_end();
+    switch (current_tab) {
+      case DASHBOARD:
+        render_tasks();
+        break;
+      case NEW_TASK:
+        /* TODO */
+        break;
     }
 
     lf_div_end();
